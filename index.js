c341e2e949325acd454ae7a9cb8445c11594fb32
Armando Santiago <armando.deveeloper@gmail.com>
2022-12-11 09:49:48 -0600

    fix: :bug: fechas con hora de verano
    Se corrige error cuando se seleccionan fechas con horario de verano

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function turno() {
     const turno = document.getElementById('turno').value;
     const fecha = document.getElementById('fecha').value;
     const fechaSplit = fecha.split('-');    
-    const hoySinHoras = new Date(hoy.getFullYear()+"-"+(hoy.getMonth() + 1)+(hoy.getDate() >= 10 ? "-" : "-0")+hoy.getDate()+ "T00:00:00")
-    const fechaTurno = new Date(fecha ? fechaSplit[0]+"-"+fechaSplit[1]+"-"+fechaSplit[2]+ "T00:00:00" : hoySinHoras);
-    const diff = fechaTurno - hoySinHoras;
+    const fechaHoySinHoras = new Date(hoy.getFullYear()+"-"+(hoy.getMonth() + 1)+(hoy.getDate() >= 10 ? "-" : "-0")+hoy.getDate()+ "T00:00:00.000Z")
+    const fechaTurno = new Date(!fecha ? fechaHoySinHoras : fechaSplit[0]+"-"+fechaSplit[1]+"-"+fechaSplit[2]+ "T00:00:00.000Z");
+    const diff = fechaTurno - fechaHoySinHoras;
     const diffDys = diff / (1000*60*60*24);
     const siguieteTurno = (diffDys % 3) + (+turno);
     asignarTurno(siguieteTurno);
